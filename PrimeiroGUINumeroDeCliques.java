05c335d5daed280d76e852b2240b6e573134c8a3
Maria <99996886+mariaclaramoura@users.noreply.github.com>
2022-03-31 19:35:48 -0300

    Rename PrimeiroGUINumeroDeCliques.txt to PrimeiroGUINumeroDeCliques.java

--- PrimeiroGUINumeroDeCliques.java
+++ PrimeiroGUINumeroDeCliques.java
@@ -19,14 +19,14 @@ public class GUI implements ActionListener {
 	
 	public GUI() {
 		
-		/*Constrói um novo frame que inicialmente é invisível*/
+		/*ConstrÃ³i um novo frame que inicialmente Ã© invisÃ­vel*/
 		quadro = new JFrame();
-		/*Constrói um novo painel*/
+		/*ConstrÃ³i um novo painel*/
 		painel = new JPanel();
-		/*Constroi um novo botão*/
+		/*Constroi um novo botÃ£o*/
 		botao = new JButton("Clique aqui");
 		
-		label = new JLabel("Números de clique: 0");		
+		label = new JLabel("NÃºmeros de clique: 0");		
 		
 		painel.setBorder(BorderFactory.createEmptyBorder(50, 50, 20, 50));
 		painel.setLayout(new GridLayout(0, 1));
@@ -36,7 +36,7 @@ public class GUI implements ActionListener {
 
 		quadro.add(painel, BorderLayout.CENTER);
 		quadro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		quadro.setTitle("Esse é o teste para fazer um GUI! :)");
+		quadro.setTitle("Esse Ã© o teste para fazer um GUI! :)");
 		quadro.pack();
 		quadro.setVisible(true);
 		
@@ -54,7 +54,7 @@ public class GUI implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		contador++;
-		label.setText("Números de clique: " + contador);
+		label.setText("NÃºmeros de clique: " + contador);
 		
 	}
 
